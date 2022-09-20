93c8862206ed2a0894c0d26d9f47cb80c7f5f590
qwertycho <tychovopstal@gmail.com>
2022-09-20 23:21:29 +0200

    db connecties

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,10 +4,12 @@ const express = require("express");
 const router = express.Router();
 const cookieParser = require("cookie-parser");
 const mariadb = require("mariadb");
+const { response } = require("express");
 
 // einde benodigde troep importeren
 router.use(cookieParser());
 
+// 
 const pool = mariadb.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -16,100 +18,68 @@ const pool = mariadb.createPool({
   connectionLimit: 5
 });
 
-async function checkUser(userName) {
-      console.log("ooooooooooooooooooooooooooooooooooooooo");
+// functie om de database te connecten
+// gebruikt const pool = mariadb.createPool
+// haat 
+async function checkUser(userName, password) {
         let conn;
         try {
+        // connectie maken met de database
         conn = await pool.getConnection();
-        console.log("pool.getConnection");
-        const res = await conn.query("SELECT * FROM gebruikers WHERE username = ?", [userName]);
+        // check of de gebruiker bestaat
+        const row = await conn.query("SELECT * FROM gebruikers WHERE username = ?", [userName]);
         console.log("conn.query");
-        console.log(res);
-      console.log("ooooooooooooooooooooooooooooooooooooooo");
+        console.log(row);
       } catch (err) {
         console.log("db error");
         console.log(err);
       throw err;
       } finally {
-        console.log("finally");
       if (conn) return conn.end();
-      console.log("return conn.end");
-      return res;
+        if(row[0].password == password){
+          return true;
+        }
       }
 }
-
-// stackoverflow
-// let getSomeDataFromDB = function(){
-//   return new Promise(function(resolve, reject){
-//     mariadb.createConnection([skip]).then(conn => {
-//       conn.query("SELECT 1 as val")
-//         .then(rows=>resolve(rows))
-//         .catch(e=>reject(e))
-//         .then(()=>conn.close())
-//     }).catch(e => reject(e))
-//   })
-// }
-// let rows = await getSomeDataFromDB()
 
 // dit is de route voor login
 // doordat dit bestand pas word aangesproken als de gebruiker naar /dashboard gaat is /login dus /dashboard/login
 // Dit is een post route en geen get route. Hier word info dus op geplaats. In dit geval de login data
 // Nu word gecheckt of de login data overeenkomt met wat in het dotenv bestand staat
 router.post("/login", (req, res) => {
-  console.log(`post/login req.cookies zijn: ${req.cookies.username}`);
-  let username = req.body.username;
-  let password = req.body.password;
-  let DbResonse = main(username);
   if (username != undefined) {
-    if (DbResonse.username == username && DbResonse.password == password) {
-      // als de login data klopt word de gebruiker door gestuurd naar de dashboard pagina
-      // de gebruiker word ook een cookie gegeven met de naam "login" en de waarde van het juiste wachtwoord
-      res.cookie("auth", password, { maxAge: 900000, httpOnly: true });
-      res.cookie("user", username, { maxAge: 900000, httpOnly: true });
-      res.redirect("/dashboard");
-    } else {
-      console.log("ooooooooooooooooooooo");
-      console.log("wrong credentials");
-      console.log(`username: ${username}`);
-      console.log(`password: ${password}`);
-      console.log(`db username: ${DbResonse.username}`);
-      console.log(`db password: ${DbResonse.password}`);
-      console.log(DbResonse);
-      console.log("ooooooooooooooooooooo");
-
-      // loggen naar database
-    }
+    checkUser(req.body.username, req.body.password).then( (authorised) => {
+      if (authorised) {
+          // als de login data klopt word de gebruiker door gestuurd naar de dashboard pagina
+          // de gebruiker word ook een cookie gegeven met de naam "login" en de waarde van het juiste wachtwoord
+          res.cookie("auth", password, { maxAge: 900000, httpOnly: true });
+          res.cookie("user", username, { maxAge: 900000, httpOnly: true });
+          res.redirect("/dashboard");
+        } else {
+          console.log("wrong credentials");
+        }
+    });
   } else {
-    console.log("no username");
-  }
+      console.log("no username");
+    }
 });
 
-// router.get("/login", (req, res) => {
-//     console.log("xxxxxxxxxxxxxxxxxxxxxx");
-//     console.log(`/login req.cookies zijn: ${req.cookies.username}`);
-//     console.log("xxxxxxxxxxxxxxxxxxxxxx");
-//     if (req.cookies.username != undefined) {
-//         let DbResonse = main(req.cookies.user);
-//         if(
-//             DbResonse.username == req.cookies.user &&
-//             DbResonse.password == req.cookies.auth
-//         ){
-//             console.log("logged in");
-//             let user = {
-//                 username: req.cookies.user,
-//             };
-//             res.render("../views/dashboard", { user: user });
-//         }
-//     } else {
-//         console.log("not logged in");
-//         res.render("../views/login", {});
-//     }
-// });
-
 router.get("/login", (req, res) => {
-  console.log("/login");
-  checkUser("qwertycho").then(res.send("hallo "));
-
+  if (req.cookies.username != undefined) {
+    checkUser(req.cookies.username, req.cookies.auth).then( (authorised) => {
+      if (authorised) {
+          // als de login data klopt word de gebruiker door gestuurd naar de dashboard pagina
+          // de gebruiker word ook een cookie gegeven met de naam "login" en de waarde van het juiste wachtwoord
+          res.cookie("auth", password, { maxAge: 900000, httpOnly: true });
+          res.cookie("user", username, { maxAge: 900000, httpOnly: true });
+          res.redirect("/dashboard");
+        } else {
+          console.log("wrong credentials");
+        }
+    });
+  } else {
+      console.log("no username");
+    }
 });
 
 // route naar de dashboard. Ofte wel /dasboard/
@@ -119,29 +89,16 @@ router.get("/login", (req, res) => {
 router.get("/", (req, res) => {
   console.log(`/ req.cookies zijn: ${req.cookies.username}`);
   if (req.cookies.username != undefined) {
-    let DbResonse = main(req.cookies.user);
-    if (
-      DbResonse.username == req.cookies.user &&
-      DbResonse.password == req.cookies.auth
-    ) {
+    checkUser(req.cookies.username, req.cookies.auth).then( (authorised) => {
+    if (authorised) {
       console.log("logged in");
-      let user = {
-        username: req.cookies.user,
-      };
-      res.render("../views/dashboard", { user: user });
+      res.render("../views/dashboard", { user: req.cookies.username });
     }
+  });
   } else {
     console.log("not logged in");
     res.render("../views/login", {});
   }
 });
-
-// async function login(username) {
-//    return new Promise((resolve, reject) => {
-//         main(username).then((result) => {
-//             resolve(result);
-//         });
-//     })
-// }
 
 module.exports = router;
