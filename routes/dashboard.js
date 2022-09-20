91ff746c4f4ed0ad893f66750f0d65ddeea93735
qwertycho <tychovopstal@gmail.com>
2022-09-20 19:12:08 +0200

    nam

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -12,7 +12,7 @@ mariadb.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: "admin_",
   connectionLimit: 5,
 })
   .then((conn) => {
