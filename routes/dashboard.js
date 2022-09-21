9bdeb5d6a0e87695b1a36262b34c85732926b201
qwertycho <tychovopstal@gmail.com>
2022-09-21 13:38:04 +0200

    try catch

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -9,6 +9,8 @@ const { response } = require("express");
 // einde benodigde troep importeren
 router.use(cookieParser());
 
+// pool maken voor de database connectie
+// todo: verplaatsen naar een aparte file
 const pool = mariadb.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -29,9 +31,10 @@ async function checkUser(userName, password) {
         console.log(row);
         console.log("conn.query");
 
+        // als de gebruiker niet bestaat in de database is de row leeg
         if (row.length == 0) {
           console.log("gebruiker bestaat niet");
-            return false;
+          return false;
         } else {
           if(row[0].password == password){
             console.log("passwords match");
@@ -43,8 +46,8 @@ async function checkUser(userName, password) {
            return false;
           }
         }
-
       } catch (err) {
+        // als er een error is, log deze dan
         console.log("db error");
         console.log(err);
       throw err;
@@ -57,48 +60,58 @@ async function checkUser(userName, password) {
 // Nu word gecheckt of de login data overeenkomt met wat in het dotenv bestand staat
 router.post("/login", (req, res) => {
   console.log("post login");
-  console.log(req.body);
-  if (req.body.username != undefined) {
-    checkUser(req.body.username, req.body.password).then(authorised => { 
-      console.log("authenticating");
-      console.log(authorised); 
-      if (authorised) {
-          // als de login data klopt word de gebruiker door gestuurd naar de dashboard pagina
-          // de gebruiker word ook een cookie gegeven met de naam "login" en de waarde van het juiste wachtwoord
-          res.cookie("auth", req.body.password, { maxAge: 900000, httpOnly: true });
-          res.cookie("user", req.body.username, { maxAge: 900000, httpOnly: true });
-          res.redirect("/dashboard");
-        } else {
-          console.log("wrong credentials");
-          console.log(req.body.username);
-          console.log(req.body.password);
-          res.status(401).send("wrong credentials");
-        }
-    });
-  } else {
-    console.log("no credentials");
-      res.send("wrong password");
-    }
+  try{
+    if (req.body.username != undefined) {
+      checkUser(req.body.username, req.body.password).then(authorised => { 
+        if (authorised) {
+            // als de login data klopt word de gebruiker door gestuurd naar de dashboard pagina
+            // de gebruiker word ook een cookie gegeven met de naam "login" en de waarde van het juiste wachtwoord
+            res.cookie("auth", req.body.password, { maxAge: 900000, httpOnly: true });
+            res.cookie("user", req.body.username, { maxAge: 900000, httpOnly: true });
+            res.redirect("/dashboard");
+          } else {
+            // als de login data niet klopt word er een error gegeven
+            console.log("wrong credentials");
+            res.status(401).send("wrong credentials");
+          }
+      });
+    } else {
+      // als er geen cookies zijn word de gebruiker door gestuurd naar de login pagina
+      console.log("no credentials");
+      res.status(401).send("no credentials");
+      }
+  } catch (err) {
+    // als er een error is, log deze dan
+    console.log("error");
+    console.log(err);
+  }
 });
 
+// de route voor dashboard/login
+// hier word gekeken of de gebruiker al is ingelogd
+// als de gebruiker al is ingelogd word hij door gestuurd naar de dashboard pagina
 router.get("/login", (req, res) => {
   console.log("get login");
-  console.log(req.cookies.user);
-  console.log(req.cookies.auth);
-  if (req.cookies.username != undefined) {
-    checkUser(req.cookies.user, req.cookies.auth).then(authorised => { 
-      if (authorised) {
-          // als de login data klopt word de gebruiker door gestuurd naar de dashboard pagina
-          // de gebruiker word ook een cookie gegeven met de naam "login" en de waarde van het juiste wachtwoord
-          res.redirect("/dashboard");
-        } else {
-          console.log("wrong credentials");
-          res.send("wrong credentials");
-        }
-    });
-  } else {
-      res.render("../views/login", {});
-    }
+  try{
+    if (req.cookies.username != undefined) {
+      checkUser(req.cookies.user, req.cookies.auth).then(authorised => { 
+        if (authorised) {
+            // als de login data klopt word de gebruiker door gestuurd naar de dashboard pagina
+            // de gebruiker word ook een cookie gegeven met de naam "login" en de waarde van het juiste wachtwoord
+            res.redirect("/dashboard");
+          } else {
+            console.log("wrong credentials");
+            res.send("wrong credentials");
+          }
+      });
+    } else {
+        res.render("../views/login", {});
+      }
+  } catch (err) {
+    // als er een error is, log deze dan
+    console.log("error");
+    console.log(err);
+  }
 });
 
 // route naar de dashboard. Ofte wel /dasboard/
@@ -106,17 +119,24 @@ router.get("/login", (req, res) => {
 // als de gebruiker geen cookie heeft word hij door gestuurd naar de login pagina
 // als de gebruiker wel een cookie heeft word gecheckt of de cookie waarde overeenkomt met de waarde in het dotenv bestand
 router.get("/", (req, res) => {
-  if (req.cookies.user != undefined) {
-    checkUser(req.cookies.user, req.cookies.auth).then(authorised => { 
-      if (authorised) {
-      console.log("logged in");
-      res.render("../views/dashboard", { user: req.cookies.user });
+  console.log("get dashboard");
+  try{
+    if (req.cookies.user != undefined) {
+      checkUser(req.cookies.user, req.cookies.auth).then(authorised => { 
+        if (authorised) {
+        console.log("logged in");
+        res.render("../views/dashboard", { user: req.cookies.user });
+      }
+    });
+    } else {
+      console.log("not logged in");
+      res.render("../views/login", {});
     }
-  });
-  } else {
-    console.log("not logged in");
-    res.render("../views/login", {});
-  }
+} catch (err) {
+  // als er een error is, log deze dan
+  console.log("error");
+  console.log(err);
+}
 });
 
 module.exports = router;
