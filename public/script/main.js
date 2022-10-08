ecd600c452ab8e79b1b34d1e5c51c89ef6d7c3f0
qwertycho <tychovopstal@gmail.com>
2022-10-08 21:15:35 +0200

    het minimale werk

--- public/script/main.js
+++ public/script/main.js
@@ -3,17 +3,21 @@ function showItems(data) {
     // clear the list
     document.getElementById("verzameling").innerHTML = "";
     // add items to list
-    for (let i = 0; i < data.length; i++) {
-        let item = data[i];
-        let div = document.createElement("div");
-        let img = document.createElement("img");
-        img.backgroundImage = item.image;
-        let p = document.createElement("p");
-        p.innerHTML = item.name;
-        div.appendChild(img);
-        div.appendChild(p);
-        document.getElementById("verzameling").appendChild(div);
-    }
+    data.forEach(element => {
+       let link = document.createElement("a");
+         link.href = `/object/${element.objectID}`;
+         link.className = "object";
+         let div = document.createElement("div");
+         div.className = "item";
+         link.innerHTML = `<h3>${element.naam}</h3>`
+         let img = document.createElement("img");
+            img.src =`/media/objecten/${element.thumbnail}`;
+            img.alt = element.naam;
+            img.width = 200;
+            div.appendChild(img);
+         link.appendChild(div);
+        document.getElementById("verzameling").appendChild(link);
+    });
 }
 
 
@@ -24,9 +28,11 @@ const getVerzameling = async() => {
     xhr.send();
     xhr.onload = function () {
         if (xhr.status == 200) {
+            console.log(this.responseText);
             showItems(JSON.parse(xhr.responseText));
         } else {
             alert(xhr.responseText);
         }
     }
 }
+getVerzameling();
