1c2f759155bf1b06461c3ff0bc48c1e06ae2cf8a
qwertycho <tychovopstal@gmail.com>
2022-10-11 13:39:29 +0200

    username tonen

--- public/script/username.js
+++ public/script/username.js
@@ -1,7 +1,8 @@
-try{
-    console.log(document.cookie);
- 
-} catch (err) {
-    // handle the error
-    console.log(err);
+let cookies = document.cookie;
+
+if(cookies.includes("user=")){
+
+    let username = cookies.split("user=")[1].split(";")[0]
+    console.log(username);
+    document.querySelector(".dashboardLink").innerHTML = username;
 }
