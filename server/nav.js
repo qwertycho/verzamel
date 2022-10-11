5db739d91f4b968c410140d8a4c0ca05cdd1a6bd
Peter <101114264+JustPeter64@users.noreply.github.com>
2022-10-11 13:34:14 +0200

    logo

--- server/nav.js
+++ server/nav.js
@@ -9,7 +9,7 @@ const navBalk = {
 
         <!-- logo -->
         <a href="/" class="menu-logo">
-            <img src="/media/pf 3.jpg"/>
+            <img src="/media/logo.png"/>
         </a>
 
         <!-- menu items -->
